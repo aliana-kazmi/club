8b5eeb5e4ced7651ce5fa43b20fe88108ce070e0
Aliana K <ailyafatima2013@gmail.com>
2022-06-21 19:40:55 +0530

    Created members app for managing accounts on the website

--- myclub_site/settings.py
+++ myclub_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition-add your apps here
 
 INSTALLED_APPS = [
+    'members.apps.MembersConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
