a45920baac7303dd62689bcb3002a2906f98fe92
Aliana K <ailyafatima2013@gmail.com>
2022-06-24 16:11:33 +0530

    changed title of upcoming events

--- events/urls.py
+++ events/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('local-venues/', views.local_venues, name='show-local-venues'),
     path('search-venues/', views.search_venues, name='search-venues'),
     path('all-events/', views.all_events, name='show-events'),
-    path('search-events/', views.search_events, name='search-events'),
-    re_path(r'^events/(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/', views.upcoming_events,name='show-events-for-this-month'),
+    re_path(r'^events/(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/',views.upcoming_events, name='show-events-for-this-month'),
     re_path(r'^(?P<year>[0-9]{4})/(?P<month>0?[1-9]|1[0-2])/', views.calender_base, name='calender'),#can process the right format
     ]
